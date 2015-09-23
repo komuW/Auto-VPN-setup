f7e6a67d4ba8308debf64b4d0a6283056f026514
komu <komuw05@gmail.com>
2015-09-23 20:27:56 +0300

    for the love of cows

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ echo "starting setup/provisioning...."
 
 if [[ ! $(ansible --version 2> /dev/null) =~ 1\.6 ]]; then
         sudo apt-get update && \
+        sudo apt-get -y install fortune cowsay && \
 	sudo apt-get -y install python-software-properties && \
         sudo apt-get -y install software-properties-common && \
 	sudo add-apt-repository -y ppa:rquillo/ansible && \
